cf1e463ce9eef44dd053930290cdcc97fd8b2492
Greisson Silva <greisson@bootweb.com.br>
2020-01-22 21:41:54 -0200

    'Commit'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
+const http = require('http');
 const cors = require('cors');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://bootweb:1Df8NHr65sEDmGsS@cluster0-06ien.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
@@ -15,7 +20,8 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
+const POST = process.env.PORT || 3333
 
-app.listen(3333, () => {
+server.listen(POST, () => {
     console.log('Server Roanding.')
 });
